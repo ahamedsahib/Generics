22238c979a93d99daa581947a7f2bdb96a8ca4ae
ahamedsahib <adsahib39@gmail.com>
2021-07-08 22:40:02 +0530

    [Ahamed]Welcome to Generics Programming

---
+++ GenericsProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To generics Programming");
+        }
+    }
+}
