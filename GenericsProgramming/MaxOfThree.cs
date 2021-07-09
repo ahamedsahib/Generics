028120dbba901fca52002d69b33861967034d988
ahamedsahib <adsahib39@gmail.com>
2021-07-09 11:49:47 +0530

    [Ahamed]Find MaxOfThree Float

--- GenericsProgramming/MaxOfThree.cs
+++ GenericsProgramming/MaxOfThree.cs
@@ -36,6 +36,21 @@ namespace GenericsProgramming
                 return num3;
             }
         }
+        public float MaxOfThreeNumbersFloat(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
 
     }
 
