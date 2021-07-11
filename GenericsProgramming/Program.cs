233e1120e3f6f1aab047bb1c4887044029dc39fc
AHAMED <68241019+ahamedsahib@users.noreply.github.com>
2021-07-11 16:56:58 +0530

    Finding Max in array

--- GenericsProgramming/Program.cs
+++ GenericsProgramming/Program.cs
@@ -7,6 +7,9 @@ namespace GenericsProgramming
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To generics Programming");
+            int[] array = { 5, 8, 3, 4, 7 };
+            MaxOfThree<int> findMaxArray = new MaxOfThree<int>(array);
+            findMaxArray.FindMaxOfArray();
             
         }
     }
